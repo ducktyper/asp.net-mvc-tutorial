17ccc4c73dfdeecd930070052800b08f01da25be
ducktyper <ducktyper@gmail.com>
2016-03-11 22:27:36 +1300

    Create migration from the change
    PM> Add-Migration AddDescriptionToProducts
    It generates the migration from the changed in models
    PM> Update-Database
    
    NOTE: Always change models first and generate the migration
    automatically, otherwise, migrations will be out of the sync with models
    and you need to delete the migrations and start over.

---
+++ MvcTutorial/Migrations/201603110902184_AddDescriptionToProducts.cs
@@ -0,0 +1,18 @@
+namespace MvcTutorial.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDescriptionToProducts : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "Description", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Products", "Description");
+        }
+    }
+}
