b9c1dc0692f358203f9665809155026fc1901cde
Ducksan Cho <ducktyper@gmail.com>
2016-03-19 22:17:22 +1300

    Set validation messages in English in tests
    Validation messages can be changed by culture info.
    Set it to US so tests won't fail from non-English Windows.

--- MvcTutorial.Tests/Models/ProductTest.cs
+++ MvcTutorial.Tests/Models/ProductTest.cs
@@ -3,12 +3,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MvcTutorial;
 using MvcTutorial.Models;
 using System.ComponentModel.DataAnnotations;
+using System.Threading;
 
 namespace MvcTutorial.Tests.Models
 {
     [TestClass]
     public class ProductTest
     {
+        [TestInitialize]
+        public void SetLocal()
+        {
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = ci;
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
         [TestMethod]
         public void TestNameRequired()
         {
