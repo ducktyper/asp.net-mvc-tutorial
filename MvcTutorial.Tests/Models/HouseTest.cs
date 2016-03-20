f8ce3cae4b72108cbc8e8976c0ea7cf0a2be58a2
Ducksan Cho <ducktyper@gmail.com>
2016-03-20 20:39:06 +1300

    Create House class and setup test file

---
+++ MvcTutorial.Tests/Models/HouseTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcTutorial.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+
+namespace MvcTutorial.Tests.Models
+{
+    [TestClass]
+    public class HouseTest
+    {
+        [TestInitialize]
+        public void SetLocal()
+        {
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = ci;
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        private void AssertValidation(string message, Object model)
+        {
+            var context = new ValidationContext(model);
+            try
+           {
+                Validator.ValidateObject(model, context);
+            }
+            catch (ValidationException ext)
+            {
+                Assert.AreEqual(message, ext.Message);
+                return;
+            }
+            Assert.Fail("Expected validation errors");
+        }
+    }
+}
