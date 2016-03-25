90c6335affda37eba7b3f9bad88d367a9bc4e04b
Ducksan Cho <ducktyper@gmail.com>
2016-03-25 17:52:26 +1300

    Add custom validation [ProductKey]

---
+++ MvcTutorial.Tests/Models/ProductTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel.DataAnnotations;
+using MvcTutorial.Models;
+using System.Threading;
+
+namespace MvcTutorial.Tests.Models
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestInitialize]
+        public void SetLocal()
+        {
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = ci;
+            Thread.CurrentThread.CurrentCulture = ci;
+        }
+
+        [TestMethod]
+        public void TestValidProduct()
+        {
+            var product = ValidProduct();
+            var context = new ValidationContext(product);
+            try
+            {
+                Validator.ValidateObject(product, context, true);
+            }
+            catch (ValidationException ext)
+            {
+                Assert.Fail(ext.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestProductKey()
+        {
+            var product = ValidProduct();
+            product.ProductKey = "invalid key";
+            AssertValidation("The field ProductKey is invalid.", product);
+        }
+
+        private Product ValidProduct()
+        {
+            return new Product() {
+                ProductKey = "0000-0000-0000-0000"
+            };
+        }
+
+        private void AssertValidation(string message, Object model)
+        {
+            var context = new ValidationContext(model);
+            try
+            {
+                Validator.ValidateObject(model, context, true);
+            }
+            catch (ValidationException ext)
+            {
+                Assert.AreEqual(message, ext.Message);
+                return;
+            }
+            Assert.Fail("Expected validation errors");
+        }
+    }
+}
---
+++ MvcTutorial/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace MvcTutorial.Models
+{
+    public class Product
+    {
+        [ProductKey]
+        public string ProductKey { get; set; }
+    }
+
+    public class ProductKeyAttribute: ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return (
+                value is string &&
+                new Regex("^\\d{4}-\\d{4}-\\d{4}-\\d{4}$").IsMatch((string)value)
+            );
+        }
+    }
+}
