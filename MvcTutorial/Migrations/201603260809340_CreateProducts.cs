97f0ce632b1916a7db32a677339183f7e021dcc8
Ducksan Cho <ducktyper@gmail.com>
2016-03-26 21:13:47 +1300

    Create a migration file
    Enable migration and create a migration file representing current Model
    structures
    1. PM> Enable-Migrations -ContextTypeName
    MvcTutorial.Models.MvcTutorialContext
    2. PM> Add-Migration CreateProducts
    3. PM> Update-Database

---
+++ MvcTutorial/Migrations/201603260809340_CreateProducts.cs
@@ -0,0 +1,27 @@
+namespace MvcTutorial.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateProducts : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        PriceInCents = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
