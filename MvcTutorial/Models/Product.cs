521fef18097ad61e138cd38e16ccb0f6198c2dc9
Ducksan Cho <ducktyper@gmail.com>
2016-03-26 21:31:27 +1300

    Add ProductReview Model
    One to many relation between Product and ProductReview

--- MvcTutorial/Models/Product.cs
+++ MvcTutorial/Models/Product.cs
@@ -1,9 +1,20 @@
-﻿namespace MvcTutorial.Models
+﻿using System.Collections.Generic;
+
+namespace MvcTutorial.Models
 {
     public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int PriceInCents { get; set; }
+        public virtual ICollection<ProductReview> Reviews { get; set; }
+    }
+
+    public class ProductReview
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
     }
 }
