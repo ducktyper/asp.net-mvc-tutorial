18a9e307239d1a1d65885f667dc1c4fef3c4520b
Ducksan Cho <ducktyper@gmail.com>
2016-03-29 21:01:42 +1300

    Test one-to-many relation

---
+++ MvcTutorial.Tests/Models/ProductTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcTutorial.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MvcTutorial.Tests.Models
+{
+    [TestClass]
+    public class ProductTest
+    {
+        private MvcTutorialContext db = new MvcTutorialContext();
+
+        [TestInitialize]
+        public void InitDb()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<MvcTutorialContext>());
+            db.Database.ExecuteSqlCommand("DELETE FROM ProductReviews");
+            db.Database.ExecuteSqlCommand("DELETE FROM Products");
+        }
+
+        [TestMethod]
+        public void TestCreateProductReviewThroughProduct()
+        {
+            var product = new Product()
+            {
+                Name = "Apple",
+                PriceInCents = 1000,
+                Reviews = new List<ProductReview>()
+            };
+            var review = new ProductReview()
+            {
+                Content = "Too expensive",
+            };
+            product.Reviews.Add(review);
+            var db = new MvcTutorialContext();
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            var productReload = db.Products.Find(product.Id);
+            Assert.AreEqual(1, productReload.Reviews.Count);
+            Assert.AreEqual("Too expensive", productReload.Reviews.First().Content);
+        }
+    }
+}
